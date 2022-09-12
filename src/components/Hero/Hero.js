2c88347be5e33dde6f52e0b3c239a06c39b858e6
CRYS <mariano.christopher123@gmail.com>
2022-09-12 19:03:40 +0800

    update info

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -16,12 +16,12 @@ const Hero = () => (
         I'm Christopher Mariano
       </SectionTitle>
       <SectionText>
-        I'm a Blockchain App Developer. I build Web3 Apps with Solidity. I test
-        and deploy them using Javascript/Typescript(Hardhat Environment). I
-        mostly use Nextjs for building the UI. Please view some of my projects
-        below. App(for the App site), Code-B(For github backend), Code-F(For
-        github frontend). Please also do view my Github if you wish to view my
-        other projects and experiments.
+        I'm a Blockchain App Developer. I build and design Web3 Apps with
+        Solidity. I test and deploy them using Javascript/Typescript(Hardhat
+        Environment). I mostly use Nextjs for building the UI. Please view some
+        of my projects below. App(for the App site), Code-B(For github backend),
+        Code-F(For github frontend). Please also do view my Github if you wish
+        to view my other projects and experiments.
       </SectionText>
       <Button
         onclick={() => (window.location = "https://github.com/crys-cross")}
